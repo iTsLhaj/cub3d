0aafa702be8aa951a7c2876abd67f5c2cbeda253
LH4J <115029856+iTsLhaj@users.noreply.github.com>
2025-02-22 00:01:17 +0100

    [cub3d] fixed some norm errors + fully understood player movement!

--- src/mem_clean.c
+++ src/mem_clean.c
@@ -6,7 +6,7 @@
 /*   By: hmouhib <hmouhib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 20:36:38 by hmouhib           #+#    #+#             */
-/*   Updated: 2025/02/19 17:01:00 by hmouhib          ###   ########.fr       */
+/*   Updated: 2025/02/22 00:00:41 by hmouhib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_end(t_game *game)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < 4)
@@ -23,16 +23,13 @@ int	ft_end(t_game *game)
 		free(game->texture_buffer[i]);
 		i++;
 	}
-
 	i = 0;
 	while (i < game->map->height)
 		free(game->map->map[i++]);
 	free(game->map->map);
-
 	mlx_destroy_image(game->mlx, game->img);
 	mlx_destroy_window(game->mlx, game->win);
 	mlx_destroy_display(game->mlx);
 	free(game->mlx);
-
 	return (0);
 }
